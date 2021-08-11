bc76fd181f0d79a81567e6e8e83eb64e85e50935
Nikita <72048187+Nikita9950@users.noreply.github.com>
2021-08-11 18:48:45 +0300

    added methods in index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,34 @@
 var myCompany = new Company('My Company', '9:00 - 18:00')
-
 var officeNumberOne = new Department(1, 'Ukraine, Kharkiv, Tankopiya st.', true)
-myCompany.addDepartment(officeNumberOne)
-
 var officeNumberTwo = new Department(2, 'Ukraine, Kiyv, Hreshatik st.', false)
-myCompany.addDepartment(officeNumberTwo)
-
 var alex = new Employee('Alex', '0661234567', 1)
-myCompany.addEmployee(alex)
-
 var vasya = new Employee('Vasya', '0671234567', 1)
-myCompany.addEmployee(vasya)
-
 var petya = new Employee('Petya', '0681234567', 2)
-myCompany.addEmployee(petya)
-
 var danil = new Employee('Danil', '0931234567', 2)
-myCompany.addEmployee(danil)
-
 var david = new HeadOfDepartment('David', '0501234567', 1, 'manager')
-
 var firstClient = new Client('Jack', alex)
 
+myCompany.addDepartment(officeNumberOne)
+myCompany.addDepartment(officeNumberTwo)
+myCompany.addEmployee(alex)
+myCompany.addEmployee(vasya)
+myCompany.addEmployee(petya)
+myCompany.addEmployee(danil)
+
+console.log(myCompany.getEmployees())
+console.log(myCompany.getDepartments())
+
+alex.setDepartmentId(2)
+petya.setDepartmentId(1)
+david.setDepartmentId(2)
+console.log(david.getDepartmentId())
+myCompany.removeEmployeeByName('Danil')
+firstClient.setEmployee(vasya)
+console.log(firstClient.getEmployee());
+console.log(myCompany.getEmployeesByDepartment(1));
+console.log(myCompany.getEmployeesByDepartment(2));
+
+console.log(officeNumberOne.getAddress());
 officeNumberOne.setAddressAsync('Ukraine, Lviv, Freedom square', function (address) {
   console.log(`New address: ${address}`)
 })
